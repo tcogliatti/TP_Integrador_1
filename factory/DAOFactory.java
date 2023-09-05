55ecc46abba981272cf324ce2da9a58f494002a1
TC <tc@MacBook-Pro-de-TC.local>
2023-09-05 15:53:24 -0300

    README: MySQL configurations added

--- factory/DAOFactory.java
+++ factory/DAOFactory.java
@@ -2,21 +2,26 @@ package factory;
 
 import dao.*;
 
-public abstract  class DAOFactory {
+public abstract class DAOFactory {
 
-    public static final int MYSQL_JDBC =1;
+    public static final int MYSQL_JDBC = 1;
 
-    public static DAOFactory getDAOFactory(int DB_Factory){
-        switch(DB_Factory){
-            case MYSQL_JDBC: return new MySQLDAOFactory();
-            default: return null;
+    public static DAOFactory getDAOFactory(int DB_Factory) {
+        switch (DB_Factory) {
+            case MYSQL_JDBC:
+                return new MySQLDAOFactory();
+            default:
+                return null;
         }
     }
 
-    public  abstract InterfaceDAO getClienteDAO()throws Exception;
-    public abstract FacturaDAO getFacturaDAO() throws  Exception;
-    public abstract ProductoDAO getProductoDAO() throws  Exception;
-    public abstract FacturaProductoDAO getFacturaProductoDAO() throws  Exception;
+    public abstract InterfaceDAO getClienteDAO() throws Exception;
+
+    public abstract FacturaDAO getFacturaDAO() throws Exception;
+
+    public abstract ProductoDAO getProductoDAO() throws Exception;
+
+    public abstract FacturaProductoDAO getFacturaProductoDAO() throws Exception;
 
 
 }
